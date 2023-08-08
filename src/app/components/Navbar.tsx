b48e4a09b9ca27a66cb4b31a3f06ee64b43cd800
jbsx <jaishsingh@hotmail.com>
2023-08-08 08:20:52 +0530

    minor changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="bg-[var(--accent-clr1)] flex justify-between items-center p-3 text-center text-[var(--accent-clr2)] font-bold text-xl px-[4em]">
-      <div className="flex items-center">
-        <img className="h-[120px]" src={Logo.src} alt="Logo" />
-        <span className="text-4xl m-[1em]">LovingTails</span>
-      </div>
+      <Link href={"/"}>
+        <div className="flex items-center">
+          <img className="h-[120px]" src={Logo.src} alt="Logo" />
+          <span className="text-4xl m-[1em]">LovingTails</span>
+        </div>
+      </Link>
 
       <div>
         <Link className="p-3 m-2 w-[100] hover:bg-white/[0.3]" href={"/store"}>
