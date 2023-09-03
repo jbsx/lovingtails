054f3940e0c5c77a41d4cfa91b4a10841f1b3c73
jbsx <jaishsingh@hotmail.com>
2023-09-03 16:33:40 +0530

    removed hamburger menu. SEO optimizations. removed use-client

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,25 +1,14 @@
-"use client";
-
 import Logo from "../../../public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { Mulish } from "next/font/google";
-import { Menu, Cross } from "../imports";
-import { useState } from "react";
 
 const mulish = Mulish({ subsets: ["latin"] });
 
 export default function Navbar() {
-  const [hmenu, setHmenu] = useState(true);
-
   return (
     <div className="flex justify-around items-center p-4 text-center text-[var(--accent-clr2)] font-bold text-xl px-4 w-screen">
-      <Link
-        href={"/"}
-        onClick={() => {
-          setHmenu(true);
-        }}
-      >
+      <Link href={"/"}>
         <div className="flex items-center">
           <Image
             width={100}
@@ -28,70 +17,26 @@ export default function Navbar() {
             alt="Logo"
             className="w-[80px]"
           />
-          <h1 className={`text-5xl m-[1em] ${mulish.className} sm:hidden`}>
+          <h1
+className={`text-5xl m-[1em] ${mulish.className} md:text-4xl sm:text-3xl`}
+          >
             LovingTails
           </h1>
         </div>
       </Link>
-      <div className="lg:hidden text-2xl font-medium uppercase">
-        <Link
-          className="p-3 m-2 w-[100] transition-opacity"
-          href={"/store"}
-          onClick={() => {
-            setHmenu(true);
-          }}
-        >
+      <div className="lg:hidden flex text-2xl font-medium uppercase transition-all">
+        <Link className="p-3 m-2 w-[100] group transition-all" href={"/store"}>
           Store
+          <span className="block max-w-0 group-hover:max-w-full duration-300 h-0.5 bg-[var(--accent-clr1)]"></span>
         </Link>
         <Link
-          className="p-3 m-2 w-[100]"
+          className="p-3 m-2 w-[100] group transition-all"
           href={"/contacts"}
-          onClick={() => {
-            setHmenu(true);
-          }}
         >
           Contacts
+          <span className="block max-w-0 group-hover:max-w-full duration-300 h-0.5 bg-[var(--accent-clr1)]"></span>
         </Link>
       </div>
-      <div className="hidden lg:block">
-        {hmenu ? (
-          <Menu
-            isVisible={hmenu}
-            onClick={() => {
-              setHmenu(!hmenu);
-            }}
-          />
-        ) : (
-          <Cross
-            isVisible={!hmenu}
-            onClick={() => {
-              setHmenu(!hmenu);
-            }}
-          />
-        )}
-      </div>
-      {!hmenu && (
-        <div className="absolute top-[152px] left-0 w-full h-full backdrop-blur-xl pt-[100px] flex flex-col text-4xl z-[999] text-[var(--accent-clr1)]">
-          <Link
-            className="p-3 m-2 w-[100]"
-            href={"/store"}
-            onClick={() => {
-              setHmenu(!hmenu);
-            }}
-          >
-            Store
-          </Link>
-          <Link
-            className="p-3 m-2 w-[100]"
-            href={"/contacts"}
-            onClick={() => {
-              setHmenu(!hmenu);
-            }}
-          >
-            Contacts
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,7 +1,5 @@
-"use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 import { dbType } from "../utils";
 
 interface Props {
@@ -9,36 +7,22 @@ interface Props {
 }
 
 export default function Product({ data }: Props) {
-  const [hover, setHover] = useState(false);
-
   return (
     <Link href={`/store/${data.name}`}>
       <div
         className="relative flex flex-col justify-center items-center w-[400px] h-[550px] p-[1em]
                    hover:border hover:border-[var(--accent-clr1)] cursor-pointer text-zinc-700 bg-[rgba(0,0,0,0.03)]
-                hover:bg-[var(--backgound-hex)] hover:text-[var(--accent-clr1)] sm:w-screen sm:h-fit sm:pb-[50px]"
-        onMouseEnter={() => {
-          setHover(true);
-        }}
-        onMouseLeave={() => {
-          setHover(false);
-        }}
+                hover:bg-[var(--backgound-hex)] hover:text-[var(--accent-clr1)] sm:w-screen sm:h-fit sm:pb-[50px] group"
       >
         <Image
           width={300}
           height={300}
-          src={require(`../../../tempdb/products/${data.name}/${
-            hover ? "2" : "1"
-          }.jpg`)}
+          src={require(`../../../tempdb/products/${data.name}/2.jpg`)}
           alt={data.name}
           className="overflow-hidden mix-blend-darken max-h-[300px]"
         />
 
-        <div
-          className={`${
-            hover ? "translate-y-[-20px]" : ""
-          } transition-all w-full`}
-        >
+        <div className="group-hover:translate-y-[-20px] transition-all w-full">
           <h1 className="text-xl font-medium my-[1em] overflow-hidden">
             {data.name}
           </h1>
@@ -49,9 +33,8 @@ export default function Product({ data }: Props) {
         </div>
         {data.tag && (
           <div
-            className={`absolute bottom-0 left-0 text-md w-full ${
-              hover ? "bg-[var(--accent-clr1)]" : "bg-[var(--accent-clr2)]"
-            } text-white font-medium p-[0.2em] px-[1em]`}
+            className="absolute bottom-0 left-0 text-md w-full group-hover:bg-[var(--accent-clr1)] 
+                        bg-[var(--accent-clr2)] text-white font-medium p-[0.2em] px-[1em]"
           >
             {data.tag}
           </div>
